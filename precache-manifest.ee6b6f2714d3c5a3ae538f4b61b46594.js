84c3a30e28add5246376eb112e1c183bb426e129
Denzel <denzeltlee@gmail.com>
2020-07-04 16:32:36 +0800

    Updates

---
+++ precache-manifest.ee6b6f2714d3c5a3ae538f4b61b46594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af164683d869b167de233e7635cecab5",
+    "url": "/react-meme-generator/index.html"
+  },
+  {
+    "revision": "e5627a367029029aa4bc",
+    "url": "/react-meme-generator/static/css/main.162b6a36.chunk.css"
+  },
+  {
+    "revision": "6cde640b58717ed0a011",
+    "url": "/react-meme-generator/static/js/2.a6415996.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-meme-generator/static/js/2.a6415996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5627a367029029aa4bc",
+    "url": "/react-meme-generator/static/js/main.2239a282.chunk.js"
+  },
+  {
+    "revision": "a252d0789912461d7403",
+    "url": "/react-meme-generator/static/js/runtime-main.ebf92e4e.js"
+  }
+]);
